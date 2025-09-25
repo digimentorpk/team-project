516f6e02f9a2a100dee96da1bbfc461a5af8f7f1
Bilal Hassan <bilal6322800@gmail.com>
2025-09-25 07:15:37 +0200

    feat(Main content) add application logic

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// MAIN APPLICATION
+// Main Application
+class app {
+constructor() {
+this.init();
+}
+init() {
+console.log('App Initialized')
+this.loadContent();
+}
+loadContent() {
+const main = document.queryselector('main');
+main.innerHTML = '<h2>Welocome to Our Application</h2><p>This is my first Js Script for Devops Learning</p>';
+}
+}
